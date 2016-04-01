b9705184bf455430242a4b61ca5195bc07059f92
hikkikuma <loli.is.me.j.pop@gmail.com>
2016-04-01 15:25:39 +0800

    format code

--- src/instruction.h
+++ src/instruction.h
@@ -3,71 +3,72 @@
 #include <cstdlib>
 #include "__type.hh"
 
-namespace X86Instruction{
+namespace X86Instruction
+{
 
-  
-  enum
-    {
-      CSa,
-      SSa,
-      DSa,
-      ESa,
-      FSa,
-      GSa,
-    };
-  
-  class InstructionPrefix
-  {
-  public:
-    CpuRegisterType::Byte group[4] = {0,0,0,0};
-  
-    //group1
-    bool lock;
-    bool rep;
-    bool repne;
-    //group 2
-    CpuRegisterType::u8 seg;
-    //group 3
-    CpuRegisterType::u8 op;
-    //group 4
-    CpuRegisterType::u8 addr;
 
-    InstructionPrefix() = default;
-    
-    InstructionPrefix(const CpuRegisterType::Byte * byte)
-    {
-      const CpuRegisterType::Byte * index = byte;
-      for (int i = 0; i < 3; ++i)
+	enum
 	{
-	  group[i] = * index;
-	  index++;
-	}
-    }
-  };
-  
+		CSa,
+		SSa,
+		DSa,
+		ESa,
+		FSa,
+		GSa,
+	};
 
-  class ModrmSib
-  {
-  public:
-    CpuRegisterType::Byte modrm;
-    CpuRegisterType::Byte sib;
-  };
+	class InstructionPrefix
+	{
+	public:
+		CpuRegisterType::Byte group[4] = { 0,0,0,0 };
 
-  class Instruction
-  {
-  private:
-    CpuRegisterType::Byte * curinst;
-    InstructionPrefix prefix;
-    ModrmSib modrm_sib;
-  public:
-    Instruction() = default;
-    Instruction(CpuRegisterType::Byte * cur) : curinst(0),  prefix(), modrm_sib()
-    {
-      curinst = cur;
-      prefix = InstructionPrefix(curinst);
-    }
-    
-  };
+		//group1
+		bool lock;
+		bool rep;
+		bool repne;
+		//group 2
+		CpuRegisterType::u8 seg;
+		//group 3
+		CpuRegisterType::u8 op;
+		//group 4
+		CpuRegisterType::u8 addr;
+
+		InstructionPrefix() = default;
+
+		InstructionPrefix(const CpuRegisterType::Byte * byte)
+		{
+			const CpuRegisterType::Byte * index = byte;
+			for (int i = 0; i < 3; ++i)
+			{
+				group[i] = *index;
+				index++;
+			}
+		}
+	};
+
+
+	class ModrmSib
+	{
+	public:
+		CpuRegisterType::Byte modrm;
+		CpuRegisterType::Byte sib;
+	};
+
+	class Instruction
+	{
+	private:
+		CpuRegisterType::Byte * curinst;
+		InstructionPrefix prefix;
+		ModrmSib modrm_sib;
+	public:
+		Instruction() = default;
+		Instruction(CpuRegisterType::Byte * cur) : curinst(0), prefix(), modrm_sib()
+		{
+			curinst = cur;
+			prefix = InstructionPrefix(curinst);
+		}
+
+	};
 }
 #endif /* CPUEMULATOR_INSTRUCTION_H_ */
 
