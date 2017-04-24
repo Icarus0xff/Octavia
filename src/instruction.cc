105cd066541eb7970ddf7646965552eb68977e8b
ghost <loli.is.me.j.pop@gmail.com>
2017-04-24 11:27:05 -0400

    format

--- src/instruction.cc
+++ src/instruction.cc
@@ -1,59 +1,43 @@
 #include "instruction.h"
 #include <cstdio>
 
-void X86Instruction::Instruction::exec_lea()
-{
-  if (modrm_sib.is_rm_a_reg)
-    {
-      printf("#UD exception\n");
-      exit(0);
+void X86Instruction::Instruction::exec_lea() {
+  if (modrm_sib.is_rm_a_reg) {
+    printf("#UD exception\n");
+    exit(0);
+  }
+  if (prefix.is_opsize16) {
+    if (prefix.is_addrsize16) {
+      general_reg[modrm_sib.reg_dest].rx = modrm_sib.effective_address_16;
+    } else {
+      general_reg[modrm_sib.reg_dest].rx = modrm_sib.effective_address_32;
     }
-  if (prefix.is_opsize16)
-    {
-      if (prefix.is_addrsize16)
-	{
-	  general_reg[modrm_sib.reg_dest].rx = modrm_sib.effective_address_16;
-	}
-      else
-	{
-	  general_reg[modrm_sib.reg_dest].rx = modrm_sib.effective_address_32;
-	}
-      
+
+  } else {
+    if (prefix.is_addrsize16) {
+      CpuRegisterType::Dword temp = 0x0000ffff & modrm_sib.effective_address_16;
+      general_reg[modrm_sib.reg_dest].erx = temp;
+    } else {
+      general_reg[modrm_sib.reg_dest].erx = modrm_sib.effective_address_32;
     }
-  else
-    {
-      if (prefix.is_addrsize16)
-	{
-	  CpuRegisterType::Dword temp = 0x0000ffff & modrm_sib.effective_address_16;
-	  general_reg[modrm_sib.reg_dest].erx = temp;
-	}
-      else
-	{
-	  general_reg[modrm_sib.reg_dest].erx = modrm_sib.effective_address_32;
-	}
-    }
+  }
 }
 
-void X86Instruction::Instruction::dec_opcode()
-{
-  switch(opcode[0])
-    {
-    case 0x8D:
-      modrm_sib = ModrmSib(curinst);    
-      exec_lea();
-      break;
-    default:
-      printf("#UD\n");
-      return;
-    }
+void X86Instruction::Instruction::dec_opcode() {
+  switch (opcode[0]) {
+  case 0x8D:
+    modrm_sib = ModrmSib(curinst);
+    exec_lea();
+    break;
+  default:
+    printf("#UD\n");
+    return;
+  }
 }
 
-void X86Instruction::Instruction::exec_inst()
-{
-  if (!prefix.is_opbyte2)
-    {
-      dec_opcode();
-      return;
-    }
-
+void X86Instruction::Instruction::exec_inst() {
+  if (!prefix.is_opbyte2) {
+    dec_opcode();
+    return;
+  }
 }
